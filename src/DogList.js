4a80e5b8107d76c0d1f2f9e5178b8de1530915c8
github username <therealgracechoi@gmail.com>
2020-03-29 09:22:52 -0700

    began implementing bootstrap into DogList, centering 'h1'

--- src/DogList.js
+++ src/DogList.js
@@ -2,8 +2,8 @@ import React, {Component} from 'react';
 
 class DogList extends Component {
     render() {
-        return <div>
-            <h1>Dog List!</h1>
+        return <div className="DogList">
+            <h1 className="display-1 text-center">Dog List!</h1>
         </div>
     }
 }
