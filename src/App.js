6ff3aefd431e10b0a4596edcfeb0973b94430589
github username <therealgracechoi@gmail.com>
2020-03-30 22:16:28 -0700

    created getDog() and passed props down to DogDetails.js

--- src/App.js
+++ src/App.js
@@ -45,10 +45,18 @@ class App extends Component {
   };
 
   render() {
+    const getDog = props => {
+      let name = props.match.params.name;
+      let currentDog = this.props.dogs.find(
+      dog => dog.name.toLowerCase() === name.toLowerCase()
+      );
+      return <DogDetails {...props} dog={currentDog}/>
+    }
+
     return (
       <Switch>
         <Route exact path='/dogs' render={() => <DogList dogs={this.props.dogs}/>} />
-        <Route exact path='/dogs/:name' render={() => <DogDetails />} />
+        <Route exact path='/dogs/:name' render={getDog} />
       </Switch>
     );
   }
--- src/DogDetails.js
+++ src/DogDetails.js
@@ -4,7 +4,7 @@ class DogDetails extends Component {
     render() {
         return (
             <div>
-
+                <h1>{this.props.dog.name}</h1>
             </div>
         )
     }
